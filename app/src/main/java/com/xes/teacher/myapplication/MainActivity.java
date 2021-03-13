d61a3274e9bf70d441560ff6085df7794f4f051b
WangQing <wangqing11@100tal.com>
2021-03-13 22:12:20 +0800

    1. 删除单元测试的相关依赖和代码； 2. 添加申请权限的接口。

--- app/src/main/java/com/xes/teacher/myapplication/MainActivity.java
+++ app/src/main/java/com/xes/teacher/myapplication/MainActivity.java
@@ -1,10 +1,10 @@
 package com.xes.teacher.myapplication;
 
 import android.accessibilityservice.AccessibilityServiceInfo;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.view.accessibility.AccessibilityManager;
 import android.widget.Toast;
@@ -15,6 +15,10 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.PermissionUtils;
+import com.blankj.utilcode.util.ShellUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.xes.teacher.myapplication.databinding.ActivityMainBinding;
 
 import java.util.List;
@@ -43,11 +47,13 @@ public class MainActivity extends AppCompatActivity {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
 
-                ComponentName componentName = new ComponentName("com.android.settings", "com.android.settings.Settings$AccessibilitySettingsActivity");
-//
-                Intent intent = new Intent();
-                intent.setComponent(componentName);
-                startActivity(intent);
+//                ComponentName componentName = new ComponentName("com.android.settings", "com.android.settings.Settings$AccessibilitySettingsActivity");
+////
+//                Intent intent = new Intent();
+//                intent.setComponent(componentName);
+//                startActivity(intent);
+
+                initAccessibilityService();
             }
         });
 
@@ -55,6 +61,20 @@ public class MainActivity extends AppCompatActivity {
 //        startService(new Intent(this, WQAccessibilityService.class));
 
         Toast.makeText(this, "障碍开关：" + isStartAccessibilityServiceEnable(this), Toast.LENGTH_SHORT).show();
+
+        PermissionUtils.requestWriteSettings(new PermissionUtils.SimpleCallback() {
+            @Override
+            public void onGranted() {
+                ToastUtils.showLong("获取修改系统权限成功");
+            }
+
+            @Override
+            public void onDenied() {
+                ToastUtils.showLong("获取修改系统权限失败");
+            }
+        });
+
+//        PermissionUtils.permission("android.permission.READ_PHONE_STATE", "android.permission.WRITE_SECURE_SETTINGS");
 
     }
 
@@ -79,21 +99,24 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-//    /**
-//     * 初始化无障碍
-//     */
-//    private void initAccessibilityService() {
-//        RootCmd.execCmd("pm grant camera.app.com.backward android.permission.READ_PHONE_STATE");
-//        boolean b = RootCmd.execCmd("pm grant camera.app.com.backward android.permission.WRITE_SECURE_SETTINGS");
+    /**
+     * 初始化无障碍
+     */
+    private void initAccessibilityService() {
+        ShellUtils.execCmd("pm grant camera.app.com.backward android.permission.READ_PHONE_STATE", false);
+        ShellUtils.CommandResult commandResult = ShellUtils.execCmd("pm grant camera.app.com.backward android.permission.WRITE_SECURE_SETTINGS", false);
+
+        LogUtils.d("数据是："  +commandResult.toString());
+
 //        if (b) {
-//            Log.d("system", "WRITE_SECURE_SETTINGS SUCCESS!");
-////                自动开启无障碍服务
-//            if (!ServiceUtils.isStartAccessibilityServiceEnable(this)) {
-//                Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
-//                        getPackageName() + "/" + getPackageName() + ".service.AutoClickService");
-//                Settings.Secure.putInt(getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED, 1);
-//                Log.d("system", "SETTING ACCESSIBILITY SUCCESS!");
-//            }
+            Log.d("system", "WRITE_SECURE_SETTINGS SUCCESS!");
+//                自动开启无障碍服务
+            if (!isStartAccessibilityServiceEnable(this)) {
+                Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
+                        getPackageName() + "/" + getPackageName() + ".service.AutoClickService");
+                Settings.Secure.putInt(getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED, 1);
+                Log.d("system", "SETTING ACCESSIBILITY SUCCESS!");
+            }
 //        }
-//    }
+    }
 }
